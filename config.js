0ba74755b1741fc1c49823a862508b96a058c668
aldoea <aldo.ea@hotmail.com>
2018-05-31 16:28:26 -0500

    Change npm script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+	db: process.env.MONGODB_URI || 'mongodb://heroku_xnbmxsjx:ijgmdbctsaqqgimf2bodn0qgl8@ds243728.mlab.com:43728/heroku_xnbmxsjx',
 	SECRET_TOKEN: 'miclavesecreta'
 };
